5d076f7729d3e533c8ef5aa618392b43138ba812
Facundo Iguacel <iguacelfacundo@gmail.com>
2022-05-04 03:00:54 -0300

    primera iteracion

---
+++ components/ProjectCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from "../styles/card.module.css";
+import utilStyles from "../styles/utils.module.css";
+import TechBadge from "./TechBadge";
+
+function ProjectCard({ title, description, image, href, techs = [] }) {
+  return (
+    <>
+      <Link href={href} passHref>
+        <div className={styles.card}>
+          <div className={styles.leftSection}>
+            <div className={styles.title}>
+              <Image
+                src={`https://icons.duckduckgo.com/ip3/${href.replace(/^https?:\/\//, "")}.ico`}
+                alt="favicon"
+                height={26}
+                width={26}
+                objectFit="contain"
+              />
+              <h2 className={utilStyles.headingMd}>{title}</h2>
+            </div>
+            <div className={styles.description}>{description}</div>
+            <div className={styles.techBadges}>
+              {techs.map((name) => (
+                <TechBadge key={name} title={name} />
+              ))}
+            </div>
+          </div>
+          <div className={styles.rightSection}>
+            <Image src={image} alt="screenshot" height={180} width={300} objectFit="contain" />
+          </div>
+        </div>
+      </Link>
+    </>
+  );
+}
+
+export default ProjectCard;
